8b899dbf8cc035a6b6594bc5a62c26b37baa057b
briddim <18778367288@163.com>
2024-10-10 20:05:21 +0800

    10.10.2
    加了点角色属性

--- Assets/Script/CharacterStats.cs
+++ Assets/Script/CharacterStats.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strenth;
+    [Header("Major stats")]
+    public Stat strenth;      //力量，每一点可以提供1点物理伤害
+    public Stat agility;      //敏捷，每一点可以提供1%攻速加成(40%为上限)和1%速度加成(20%为上限)
+    public Stat intelgence;   //智慧，每一点可以提供1魔法伤害
+    public Stat vitality;     //体力，每一点可以提供生命加成
+
+    [Header("Defensive stats")]
+    public Stat maxHealth;      //最大生命
+    public Stat armor;          //护甲值，每一点提供1伤害减免
+
     public Stat damage;
-    public Stat maxHealth;
 
     [SerializeField] private int currentHealth;
 
@@ -17,8 +26,23 @@ public class CharacterStats : MonoBehaviour
     public virtual void DoDamage(CharacterStats _targetStats)
     {
         int totleDamage = damage.GetValue() + strenth.GetValue();
+        totleDamage = CheckTargetArmor(_targetStats, totleDamage);
         _targetStats.TakeDamage(totleDamage);
     }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totleDamage)
+    {
+        int value = _targetStats.armor.GetValue();
+        if(value >= totleDamage)
+        {
+            return 1;
+        }
+        else
+        {
+            return totleDamage - value; 
+        }
+    }
+
     protected virtual void TakeDamage(int _damage)
     {
         if (currentHealth < _damage)
